ed9cd3d8426556db93ecfcc1f92b8874777f17d3
Ben Sokol <ben@bensokol.com>
2019-02-22 21:15:02 -0600

    Change UTL_inputValidation to use numeric_limits

--- UTL_inputValidation.hpp
+++ UTL_inputValidation.hpp
@@ -3,7 +3,7 @@
 * @Author:   Ben Sokol
 * @Email:    ben@bensokol.com
 * @Created:  February 13th, 2019 [11:24am]
-* @Modified: February 13th, 2019 [1:08pm]
+* @Modified: February 22nd, 2019 [5:01pm]
 * @Version:  1.0.0
 *
 * Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -12,14 +12,15 @@
 #ifndef UTL_INPUTVALIDATION_HPP
 #define UTL_INPUTVALIDATION_HPP
 
-#include <cstdint>
+#include <limits>
 #include <string>
 
 namespace UTL {
   // num is the string to validate
   // min is the minimum valid number
   // max is the maximum valid number
-  bool isNumber(const std::string &num, const intmax_t &min = INTMAX_MIN, const intmax_t &max = INTMAX_MAX);
+  bool isNumber(const std::string &num, const intmax_t &min = std::numeric_limits<intmax_t>::min(),
+                const intmax_t &max = std::numeric_limits<intmax_t>::max());
 }  // namespace UTL
 
 #endif
